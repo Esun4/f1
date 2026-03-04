29e79cac146de15f751aff4c9a6480dd88f1b537
Esun4 <ethansun44@gmail.com>
2026-03-03 22:26:12 -0500

    saved races 1-10

--- run_compare.py
+++ run_compare.py
@@ -6,7 +6,7 @@ from src.data_loader import get_fastest
 
 fastf1.Cache.enable_cache("/Users/ethan/Documents/coding/F1_telem/cache/fastf1")
 
-sessions = [6, 7, 8, 9, 10]
+sessions = [1, 2, 3, 4, 5]
 
 # create a loop to go through the sessions
 
