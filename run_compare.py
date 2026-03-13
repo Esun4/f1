b82956b0508d2560dd58f05de927e26aa06730d0
Esun4 <ethansun44@gmail.com>
2026-03-13 17:19:33 -0400

    performed all calculations on all 24 races in 2024

--- run_compare.py
+++ run_compare.py
@@ -6,7 +6,7 @@ from src.data_loader import get_fastest
 
 fastf1.Cache.enable_cache("/Users/ethan/Documents/coding/F1_telem/cache/fastf1")
 
-sessions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sessions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
 # create a loop to go through the sessions
 
